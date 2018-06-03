47b44e35f387a13436f00795a315f75d484f15c6
imchukwu <imchukwu@gmail.com>
2018-06-03 04:20:14 +0100

    Adding views and Firebase Authentication

---
+++ app/src/main/java/com/alumni/alumni/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.alumni.alumni;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        moveToMainScreen();
+
+    }
+
+
+    private void moveToMainScreen(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 4000);
+    }
+}
